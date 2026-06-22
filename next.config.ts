b5264687a0ee205f84bfaae98aff2b78e806bcbf
Carlos David <romao.carlosdavi@gmail.com>
2026-06-22 16:16:42 -0300

    chore: configure wiki stack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: true,
+  experimental: {
+    authInterrupts: true,
+  },
 };
 
 export default nextConfig;
